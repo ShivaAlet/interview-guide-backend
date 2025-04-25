53aa5ed7ff0c2aa070dbc9baac5dbaab13ce498d
Shiva Aleti <aletishiva218@gmail.com>
2025-04-26 00:37:22 +0530

    updated to host

--- app.py
+++ app.py
@@ -134,5 +134,5 @@ def get_guide(id):
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True)
