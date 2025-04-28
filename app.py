ece7076801360af30d1440c3451c2e9ae44a9da3
Shiva Aleti <aletishiva218@gmail.com>
2025-04-28 19:49:06 +0530

    updated port

--- app.py
+++ app.py
@@ -236,5 +236,5 @@ def delete_guide(id):
         return jsonify({"status": "Not Ok", "error": str(e)}), 400
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True)
