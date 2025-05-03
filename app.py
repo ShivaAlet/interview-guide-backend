4af45407d96c92fc84f44424d89a39dab5a9cdae
Shiva Aleti <aletishiva218@gmail.com>
2025-05-03 17:29:33 +0530

    updated api

--- app.py
+++ app.py
@@ -281,5 +281,5 @@ def mark_as_complete(guideId,mainModule,subModuleInd):
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True)
