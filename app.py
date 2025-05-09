93426589fe35d2ad833a248c019c104d6d1b4a78
Shiva Aleti <aletishiva218@gmail.com>
2025-05-09 14:48:38 +0530

    updated host

--- app.py
+++ app.py
@@ -500,5 +500,5 @@ def update_csv():
     return {"message": "CSV updated in Google Drive successfully!"}, 200
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True)
