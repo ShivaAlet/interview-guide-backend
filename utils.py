d8e4a1ff799d4b9863870a19bf8fe358caa2b5ea
Shiva Aleti <aletishiva218@gmail.com>
2025-05-13 12:24:27 +0530

    updated the model of gemini preview

--- utils.py
+++ utils.py
@@ -87,7 +87,7 @@ def generateCompanyResearchPrompt(data,module):
 
 
 def get_response(question, results,errorJsons, index):
-    model="perplexity/sonar" if(index==0 or index==1) else "google/gemini-2.0-flash-001"
+    model="perplexity/sonar" if(index==0 or index==1) else "google/gemini-2.5-flash-preview"
     print(index,model)
     while True:
         try:
